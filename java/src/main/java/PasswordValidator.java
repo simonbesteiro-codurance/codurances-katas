f8a7fbcb1fefe900a2f0ae4dbe3f0a968f1b9a04
Simon <simonbesteiro@gmail.com>
2022-01-19 18:32:18 +0100

    real finish kata

--- java/src/main/java/PasswordValidator.java
+++ java/src/main/java/PasswordValidator.java
@@ -1,4 +1,6 @@
 public class PasswordValidator {
+  private static final int MIN_CHARACTERS = 8;
+
   public static boolean isValid(String password) {
     boolean hasLowercase = false;
     boolean hasUppercase = false;
@@ -10,6 +12,36 @@ public class PasswordValidator {
       if(!hasNumber) hasNumber = isNumber(password.charAt(i));
 
     }
-    return false;
+    return hasLowercase
+      && hasUppercase
+      && hasMoreThanEightCharacters(password)
+      && hasNumber
+      && hasUnderscore(password)
+      && hasFirstCharAsCapitalLetter(password);
+  }
+
+  private static boolean isHasUppercase(char character) {
+    return Character.isUpperCase(character);
+  }
+
+  private static boolean isHasLowercase(char character) {
+    return Character.isLowerCase(character);
+  }
+
+  private static boolean isNumber(char character) {
+    return Character.isDigit(character);
+  }
+
+  private static boolean hasMoreThanEightCharacters(String password) {
+    return password.length() >= MIN_CHARACTERS;
+  }
+
+  private static boolean hasUnderscore(String password) {
+    return password.contains("_");
+  }
+
+  private static boolean hasFirstCharAsCapitalLetter(String password) {
+    return Character.isUpperCase(password.charAt(0));
   }
 }
+
--- java/src/test/java/PasswordValidatorShouldTest.java
+++ java/src/test/java/PasswordValidatorShouldTest.java
@@ -10,4 +10,33 @@ public class PasswordValidatorShouldTest {
   public void is_not_valid_when_password_is_less_than_eight_characters() {
     assertFalse(PasswordValidator.isValid("mypass"));
   }
+
+  @Test
+  public void is_not_valid_when_password_does_not_contains_an_uppercase_character() {
+    assertFalse(PasswordValidator.isValid("mypassword"));
+  }
+  @Test
+  public void is_not_valid_when_password_does_not_contains_a_lower_character() {
+    assertFalse(PasswordValidator.isValid("MYPASSWORD"));
+  }
+
+  @Test
+  public void is_not_valid_when_password_does_not_contains_a_number() {
+    assertFalse(PasswordValidator.isValid("Mypassword"));
+  }
+
+  @Test
+  public void is_not_valid_when_password_does_not_contains_an_underscore() {
+    assertFalse(PasswordValidator.isValid("Mypassword9"));
+  }
+  @Test
+  public void is_not_valid_when_password_does_not_starts_with_a_capital_character() {
+    assertFalse(PasswordValidator.isValid("mYpassword9_"));
+  }
+
+  @Test
+  public void is_valid_when_password_given_fullfilled_all_requirements() {
+    assertTrue(PasswordValidator.isValid("MYpassword9_"));
+  }
+
 }
