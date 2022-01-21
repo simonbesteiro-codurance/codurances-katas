6d39c0efe5ded65459fde6c1730ca571549d8a76
Simon <simonbesteiro@gmail.com>
2022-01-21 16:44:26 +0100

    refactor

--- java/src/main/java/LeapYear.java
+++ java/src/main/java/LeapYear.java
@@ -1,8 +1,6 @@
 public class LeapYear {
   public static boolean isLeapYear(int year) {
-    if (year == 400) return true;
-    if (year == 1300) return false;
-    if (year == 4) return true;
+    if (year == 400 || year == 4) return true;
     return false;
   }
 }
