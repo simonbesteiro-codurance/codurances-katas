45918f3ea64f2e92b836e54dd0364d85961e5472
Simon <simonbesteiro@gmail.com>
2022-01-21 17:08:38 +0100

    finish add test

--- java/src/main/java/LeapYear.java
+++ java/src/main/java/LeapYear.java
@@ -1,13 +1,13 @@
 public class LeapYear {
   public static boolean isLeapYear(int year) {
-    if (isModuleOf(year, 4)) return false;
-    else if (isModuleOf(year, 100)) return true;
-    else if (isModuleOf(year, 400)) return false;
+    if (isNotModuleOf(year, 4)) return false;
+    else if (isNotModuleOf(year, 100)) return true;
+    else if (isNotModuleOf(year, 400)) return false;
     return true;
   }
 
-  private static boolean isModuleOf(int year, int base) {
-    return year % base != 0;
+  private static boolean isNotModuleOf(int checkedNumber, int base) {
+    return checkedNumber % base != 0;
   }
 
 }
